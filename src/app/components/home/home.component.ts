efa79f8a9e74ba161c2672f0d5696b361ed7e804
Alexander <duvanhoyos@hotmail.com>
2019-02-05 15:04:54 -0500

    Busqueda artistas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,8 +14,7 @@ nuevasCanciones: any = [];
 
   constructor(private spotify: SpotifyService) {
   this.spotify.getNewReleases().subscribe((data:any) =>{
-console.log(data.albums.items);
-this.nuevasCanciones=data.albums.items;
+  this.nuevasCanciones=data;
   });
 
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,10 +16,21 @@ export class SpotifyService {
   getNewReleases() {
 
   const headers = new HttpHeaders({
-'Authorization': 'Bearer BQCGN4B85I-y6H486EoGFGOwJxI8HgoLYQdzdm-XCvx7suLzwePGYsU6URUhazEd5e2p979GjY8Iso3JwDU'
+'Authorization': 'Bearer BQCkyLbwF3vHrOqezNm4lXA1kLdbyNKbrYIBFixbpV3f_suKyhtdYwcs_KQI8yeAyi6-6kjXBycxOF-gqgKS5VYNsTBcf1QO3YIuy7MBCrGthDkav2daKH4liGxTydRRLyLuIszhXw5MF02nfenc'
   });
 
-   return this.http.get("https://api.spotify.com/v1/browse/new-releases?limit=20",{headers});
+   return this.http.get("https://api.spotify.com/v1/browse/new-releases?limit=20",{headers})
+   .pipe( map( data => data['albums'].items ));
   }
+
+  getArtista(termino: string) {
+
+    const headers = new HttpHeaders({
+  'Authorization': 'Bearer BQCkyLbwF3vHrOqezNm4lXA1kLdbyNKbrYIBFixbpV3f_suKyhtdYwcs_KQI8yeAyi6-6kjXBycxOF-gqgKS5VYNsTBcf1QO3YIuy7MBCrGthDkav2daKH4liGxTydRRLyLuIszhXw5MF02nfenc'
+    });
+  
+     return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`,{headers});
+    }
+  
 
 }
